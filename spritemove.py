9c2e24696c9d4763fd60deb09b0d2dc3cc3da275
Mansibunny <myyoga75@yahoo.com>
2016-05-31 20:49:34 -0400

    final sprites yeah!!!!!!!!!!
    finally figured it out. dude sprite with jumping and regular moving. Added the background mask thing, it all works great!

--- spritemove.py
+++ spritemove.py
@@ -5,30 +5,35 @@ This example demonstrates simple sprite animation without using any "fancy" tech
 
 """
 from pygame import *
+from random import randint
+size = width, height = 800, 500
+screen = display.set_mode(size)
+init()
+backPic = image.load("back.JPG")
 
-screen = display.set_mode((800,600))
-
-def moveMario():
+def moveDude(dude):
     ''' moveMario controls the location of Mario as well as adjusts the move and frame
         variables to ensure the right picture is drawn.
     '''
-    global move, frame, marioX, marioY
+    global move, frame
     keys = key.get_pressed()
     
 
     newMove = -1        
-    if keys[K_RIGHT]:
+    if keys[K_RIGHT] and dude[X] < 3600:
         newMove = RIGHT
-        marioX += 2
-    elif keys[K_DOWN]:
-        newMove = DOWN
-        marioY += 2
-    elif keys[K_UP]:
-        newMove = UP
-        marioY -= 2
-    elif keys[K_LEFT]:
+        dude[X] += 5
+        if dude[SCREENX] <450:
+            dude[SCREENX] += 5
+        
+    elif keys[K_LEFT] and dude[X] > 450:
         newMove = LEFT
-        marioX -= 2
+        dude[X] -= 5
+        if dude[SCREENX] <450:
+            dude[SCREENX] += 5
+    elif keys[K_SPACE] and dude[ONGROUND]:
+        dude[VY] = -8
+        dude[ONGROUND]=False
     else:
         frame = 0
 
@@ -39,7 +44,25 @@ def moveMario():
     elif newMove != -1:     # a move was selected
         move = newMove      # make that our current move
         frame = 1
-
+        
+    dude[Y]+=dude[VY]     # add current speed to Y
+    if dude[Y] >= 450:
+        dude[Y] = 450
+        dude[VY] = 0
+        dude[ONGROUND]=True
+    dude[VY]+=.2     # add current speed to Y
+    
+        
+def checkCollide(dude,plats):
+    global rec
+    rec = Rect(dude[X],dude[Y],20,31)
+    for p in plats:
+        if rec.colliderect(p):
+            if dude[VY]>0 and rec.move(0,-dude[VY]).colliderect(p)==False:
+                dude[ONGROUND]=True
+                dude[VY] = 0
+                dude[Y] = p.y - 32
+                
 def makeMove(name,start,end):
     ''' This returns a list of pictures. They must be in the folder "name"
         and start with the name "name".
@@ -51,42 +74,51 @@ def makeMove(name,start,end):
     return move
 
 
-def drawScene():
-    screen.fill((200,222,255))
+def drawScene(screen,dude):
+    offset= dude[SCREENX] - dude[X] 
+    screen.blit(backPic,(offset,0))
+    for pl in plats:
+        p = pl.move(offset,0)        
+        draw.rect(screen,(111,111,111),p)
     pic = pics[move][int(frame)]
-    screen.blit(pic,(marioX-pic.get_width()//2,marioY-pic.get_height()//2))            
+    screen.blit(pic,(dude[SCREENX]-10,dude[Y]-15))
+
     display.flip()
 
 
 RIGHT = 0 # These are just the indices of the moves
-DOWN = 1  
-UP = 2
-LEFT = 3
-RIGHT = 0 # These are just the indices of the moves
-DOWN = 1  
-UP = 2
-LEFT = 3
+LEFT = 1
+
 
 pics = []
 pics.append(makeMove("hunts",10,18))      # RIGHT
-pics.append(makeMove("hunts",77,82))     # DOWN
-pics.append(makeMove("hunts",68,73))    # UP
 pics.append(makeMove("hunts",142,150))    # LEFT
 
 frame=0     # current frame within the move
 move=0      # current move being performed
-marioX, marioY = 400,300
+
+X=0
+Y=1
+VY=2
+ONGROUND=3
+SCREENX = 4
+dude=[250,450,0,True,250]
 
 running = True
 myClock = time.Clock()
 
+plats = []
+for i in range(20):
+    plats.append(Rect(randint(100,2000),randint(250,480),60,10))
+    
 while running:
     for evnt in event.get():
         if evnt.type == QUIT:
             running = False
         
-    moveMario()          
-    drawScene()
+    moveDude(dude)
+    checkCollide(dude,plats)
+    drawScene(screen, dude)
     myClock.tick(50)
     
 quit()
